ad2cc232a7975085646031e1925190cf75ff2e7d
maxbauer <maxfbauer@googlemail.com>
2012-10-28 13:52:13 +0100

    zustände hinzugefügt

--- Buffer/src/TestBuffer.cpp
+++ Buffer/src/TestBuffer.cpp
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
 			cout << zeichen << endl;
 			x++;
 		}
+		//teststesttesestestset
 		else{
 			cout << zeichen;
 		}
--- Scanner/src/TestScanner.cpp
+++ Scanner/src/TestScanner.cpp
@@ -1,10 +1,24 @@
 #include "Scanner.h"
 
+
 int main(int argc, char **argv) {
 
 	//Scanner* scanner;
 
 	//scanner = new Scanner();
+
+	char zeichen;
+	char stringInput[] = "/home/max/myFile.txt";
+	char stringOutput[] = "/home/max/writeFile.txt";
+
+	Buffer* buffer = new Buffer(stringInput);
+	Automat* automat = new Automat();
+
+	while(!buffer->isEOF){
+		zeichen = buffer->getChar();
+
+
+	}
 
 }
 
