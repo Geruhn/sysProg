175745a2b9865b10a3a489f5ec720ecef849b088
Andy Perdana <geruhn@hotmail.com>
2012-12-05 15:00:01 +0100

    Automat aufgeraeumt, jetzt saven bevor neuester Master gepullt wird.

--- Automat/src/Automat.cpp
+++ Automat/src/Automat.cpp
@@ -6,11 +6,6 @@
  */
 
 #include "Automat.h"
-//#include "StateInterface.h"
-/*
- * Current soll Pointer sein. Zustand legt neuen Zustand in demensprechenden Übergang fest und gibt dem Automaten per setStateInterface()
- * einen Pointer auf den neuen Zustand der in Current gespeichert wird zum weiterarbeiten.
- */
 /**
  * In STATES steht die aktuelle Anzahl aller Zustände drin, sie muss vor dem
  * make-Befehl aktualisiert werden, falls sich die Anzahl der möglichen 
@@ -18,32 +13,11 @@
  */
 Automat::Automat() {
     currentState = 0;
-    //isToken = false;
-
-//    this->reg = new automatRegister(this);
-//    this->states = new StateInterface[STATES];
-//    this->states[0] = (StateInterface*) new stateStart();
-//    this->states[1] = (StateInterface*) new stateIdentifier();
-//    this->states[2] = (StateInterface*) new stateDigit();
-//
-//    this->currentState = (StateInterface*) this->states;
-//    
-//    for(int i = 0; i < this->STATES; i++) {
-//        (states+i)->startState(states, STATES);
-//    }
 }
 
 Automat::~Automat() {
 	// TODO Auto-generated destructor stub
 }
-/*
- * Funktion zum setzen des aktuellen Zustandes.
- * FYI: Erst ab Java7 ist es möglich switch-case mit einem String zu machen.
- * @param nextState Pointer to the next State.
- */
-//void Automat::setState(StateInterface* nextState) {
-//    this->currentState = nextState;
-//}
 
 /*
  * Reads a character and return an autoContainer with information about the Token.
@@ -64,9 +38,7 @@ void Automat::read(const char* c){
         default:
             break;
             
-    }
-//    this->currentContainer = this->currentState->readChar(c);
-    
+    }    
 }
 
 void Automat::ungetChar(const char* c) {
@@ -77,27 +49,6 @@ void Automat::ungetChar(const char* chars, int arrayLength) {
         //this->scanner->ungetChar((chars + i)*);
     }
 }
-
-/*
- * Function signals if a Token was found.
- * @return bool True if a new Token was found. False if there is no new Token.
- */
-/*
-bool Automat::hasToken(){
-	return isToken;
-}
- */
-
-/*
- * Function get called if a Token is found/finished.
- * @param type Type of the found Token.
- */
-/* Begründung s. hasToken.
-void Automat::setTokenFound(int type){
-	isToken = true;
-	currentContainer->setType(type);
-}
-*/
 
 /** 
  * 
